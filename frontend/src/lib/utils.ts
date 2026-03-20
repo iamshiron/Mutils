b0f3ab51f44545998fd48f08864ff6e590310301
Shiron <contact@shiron.io>
2026-03-20 15:48:10 +0100

    feat(frontend): Added shadcn components

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,19 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
-}
-
-export function getKeyTypeFromCount(
-	keyCount: number | null | undefined,
-): string | null {
-	if (keyCount === null || keyCount === undefined || keyCount < 1) {
-		return null;
-	}
-
-	if (keyCount >= 10) return "chaoskey";
-	if (keyCount >= 6) return "goldkey";
-	if (keyCount >= 3) return "silverkey";
-	return "bronzekey";
+  return twMerge(clsx(inputs))
 }
